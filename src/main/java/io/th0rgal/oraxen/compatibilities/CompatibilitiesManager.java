b0c40d3b3fb3aec6a12c316fcea7866c64632830
Phan Trọng Khang <trongkhabgphan@gmail.com>
2026-09-13 13:29:04 +0700

    remove Iris import/register from CompatibilitiesManager

--- src/main/java/io/th0rgal/oraxen/compatibilities/CompatibilitiesManager.java
+++ src/main/java/io/th0rgal/oraxen/compatibilities/CompatibilitiesManager.java
@@ -2,7 +2,6 @@ package io.th0rgal.oraxen.compatibilities;
 
 import io.th0rgal.oraxen.compatibilities.provided.blocklocker.BlockLockerCompatibility;
 import io.th0rgal.oraxen.compatibilities.provided.bossshoppro.BossShopProCompatibility;
-import io.th0rgal.oraxen.compatibilities.provided.iris.IrisCompatibility;
 import io.th0rgal.oraxen.compatibilities.provided.mythicmobs.MythicMobsCompatibility;
 import io.th0rgal.oraxen.compatibilities.provided.placeholderapi.PlaceholderAPICompatibility;
 import io.th0rgal.oraxen.compatibilities.provided.skript.SkriptCompatibility;
@@ -31,7 +30,6 @@ public class CompatibilitiesManager {
         addCompatibility("MythicMobs", MythicMobsCompatibility.class, true);
         addCompatibility("BlockLocker", BlockLockerCompatibility.class, true);
         addCompatibility("Skript", SkriptCompatibility.class, true);
-        addCompatibility("Iris", IrisCompatibility.class, true);
     }
 
     public static void disableCompatibilities() {
